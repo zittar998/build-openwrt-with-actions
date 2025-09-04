4f2cad4aab167c61488a6eaec20d29e58c90c296
ziitar <uziitar@gmail.com>
2025-09-04 20:10:17 +0800

    update

---
+++ diy.sh
@@ -0,0 +1,17 @@
+# 请在下方输入自定义命令(一般用来安装第三方插件)(可以留空)
+
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
+
+# Modify default theme
+#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+# Modify hostname
+#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+
+# Add a feed source
+#echo 'src-git kenzok8 https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+#echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+#git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
+#git clone https://github.com/kenzok8/small.git package/small
+#git clone https://github.com/SunBK201/UA3F.git package/UA3F
