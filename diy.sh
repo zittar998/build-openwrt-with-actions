b4afeccc61b1ec0d6ba5a3218e236836efc33761
ziitar <uziitar@gmail.com>
2025-09-14 15:38:23 +0800

    update

--- diy.sh
+++ diy.sh
@@ -1,19 +1,20 @@
 # 请在下方输入自定义命令(一般用来安装第三方插件)(可以留空)
 
-# Modify default IP
+# 编辑默认的lan口ip地址
 sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
 
-# Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+# 编辑默认的主题
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
-# Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+# 编辑默认的luci显示的固件名称
+#sed -i 's/OpenWrt/ZWRT/g' package/base-files/files/bin/config_generate
+#sed -i 's/ImmortalWrt/ZWRT/g' package/base-files/files/bin/config_generate
 
-# Add a feed source
+# 添加额外的软件包，echo 方式和git clone 方式二选一即可
 #echo 'src-git kenzok8 https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 #echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 echo 'src-git UA3F https://github.com/SunBK201/UA3F.git' >>feeds.conf.default
-echo 'src-git ADGH https://github.com/rufengsuixing/luci-app-adguardhome.git' >>feeds.conf.default
 #git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
 #git clone https://github.com/kenzok8/small.git package/small
 #git clone https://github.com/SunBK201/UA3F.git package/UA3F
+git clone https://github.com/kongfl888/luci-app-adguardhome.git package/ADGH
