8f6bf6dee3b0e0a1f82bc4b52bfb4baee72e0438
ziitar <uziitar@gmail.com>
2025-09-14 17:43:10 +0800

    update

--- diy.sh
+++ diy.sh
@@ -17,4 +17,4 @@ echo 'src-git UA3F https://github.com/SunBK201/UA3F.git' >>feeds.conf.default
 #git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
 #git clone https://github.com/kenzok8/small.git package/small
 #git clone https://github.com/SunBK201/UA3F.git package/UA3F
-git clone https://github.com/kongfl888/luci-app-adguardhome.git package/ADGH
+git clone https://github.com/stevenjoezhang/luci-app-adguardhome.git package/ADGH
