71f17e53d8b13188f20db11dbe39daf791185628
zittar998 <992805329@qq.com>
2025-11-14 11:18:31 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,7 +1,7 @@
 # 请在下方输入自定义命令(一般用来安装第三方插件)(可以留空)
 
 # 编辑默认的lan口ip地址
-sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 # 编辑默认的主题
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
